f3bfb5ddbf7748c6cf7ab8d52b3215c751aa0321
James Grieve <jamesonrgrieve@gmail.com>
2025-10-28 19:20:34 -0600

    Initial commit;

---
+++ example/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace example;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string userName;
+        Console.Write("Please enter your name: ");
+        userName = Console.ReadLine().Trim();
+        Console.WriteLine($"Hello, {userName}!");
+    }
+}
