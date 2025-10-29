fc748c0372a5ef21d37499c8bd073cd2de8c3777
James Grieve <jamesonrgrieve@gmail.com>
2025-10-28 19:26:30 -0600

    Decisions and loops;

--- example/Program.cs
+++ example/Program.cs
@@ -6,8 +6,19 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         string userName;
-        Console.Write("Please enter your name: ");
-        userName = Console.ReadLine().Trim();
-        Console.WriteLine($"Hello, {userName}!");
+        const string GREETING = "Hello, ";
+        // Prompt for input without newline.
+        do
+        {
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine().Trim();
+            if (userName.Length == 0)
+            {
+                Console.WriteLine("It doesn't look like you've given me a name!");
+            }
+        } while (userName.Length == 0);
+        Console.WriteLine($"{GREETING}{userName}!");
+
+
     }
 }
