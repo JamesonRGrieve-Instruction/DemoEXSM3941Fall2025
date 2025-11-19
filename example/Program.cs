f3bd3f9a0b688160c6548d467f24c1c890589c76
James Grieve <jamesonrgrieve@gmail.com>
2025-11-18 19:10:23 -0700

    Create and append to files;

--- example/Program.cs
+++ example/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace example;
+﻿using System.IO;
+
+namespace example;
 
 public class Program
 {
@@ -22,6 +24,25 @@ public class Program
                 Console.WriteLine($"{i + 1}: {names[i]} is {Math.Floor((DateOnly.FromDateTime(DateTime.Now).DayNumber - birthdates[i].DayNumber) / 365.25)}");
             }
         } while (input != "exit");
-
+        FileStream stream;
+        try
+        {
+            stream = new FileStream("data.txt", FileMode.CreateNew);
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine("name,birthdate");
+            }
+        }
+        catch
+        {
+            stream = new FileStream("data.txt", FileMode.Append);
+        }
+        using (StreamWriter writer = new StreamWriter(stream))
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                writer.WriteLine($"{names[i]},{birthdates[i]}");
+            }
+        }
     }
 }
