0c560ec3058077533efaed51449ec50c41af532f
James Grieve <jamesonrgrieve@gmail.com>
2025-11-25 19:10:49 -0700

    Sets, stacks and queues;

--- example/Program.cs
+++ example/Program.cs
@@ -6,60 +6,55 @@ public class Program
 {
     static void Main(string[] args)
     {
-        List<string> names = new List<string>(); // Physical Size
-        List<DateOnly> birthdates = new List<DateOnly>(); // Parallel Array
-        const string FILENAME = "data.txt", HEADER = "name,birthdate";
-        using (StreamReader reader = File.OpenText(FILENAME))
+        string[] names = new string[] { "Joe", "John", "Jane", "John", "Joe" };
+
+
+        // Automatically filter out duplicates.
+        HashSet<string> theHashSet = new HashSet<string>();
+
+        foreach (string name in names)
         {
-            string line = null;
-            do
-            {
-                line = reader.ReadLine();
-                if (line != null & line != HEADER)
-                {
-                    string[] splitLine = line.Split(',');
-                    names.Add(splitLine[0]);
-                    birthdates.Add(DateOnly.Parse(splitLine[1]));
-                }
-            } while (line != null);
+            theHashSet.Add(name);
+        }
+        Console.WriteLine("--- HashSet ---");
+        foreach (string name in theHashSet)
+        {
+            Console.WriteLine(name);
         }
 
-        string input = "";
-        do
+        // Automatically filter out duplicates and sort.
+        SortedSet<string> theSortedSet = new SortedSet<string>();
+
+        foreach (string name in names)
         {
-            Console.Write("Please Enter a Name or 'exit' to Exit: ");
-            input = Console.ReadLine().Trim();
-            if (input != "exit")
-            {
-                names.Add(input);
-                Console.Write($"Please Enter the Birth Date of {input} (YYYY-MM-DD): ");
-                birthdates.Add(DateOnly.Parse(Console.ReadLine().Trim()));
-            }
-            for (int i = 0; i < names.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}: {names[i]} is {Math.Floor((DateOnly.FromDateTime(DateTime.Now).DayNumber - birthdates[i].DayNumber) / 365.25)}");
-            }
-        } while (input != "exit");
-        FileStream stream;
-        // try
-        // {
-        //     stream = new FileStream(FILENAME, FileMode.CreateNew);
-        //     using (StreamWriter writer = new StreamWriter(stream))
-        //     {
-        //         writer.WriteLine(HEADER);
-        //     }
-        // }
-        // catch
-        // {
-        //     stream = new FileStream(FILENAME, FileMode.Append);
-        // }
-        using (StreamWriter writer = new StreamWriter(new FileStream(FILENAME, FileMode.Create)))
+            theSortedSet.Add(name);
+        }
+        Console.WriteLine("--- SortedSet ---");
+        foreach (string name in theSortedSet)
         {
-            writer.WriteLine(HEADER);
-            for (int i = 0; i < names.Count; i++)
-            {
-                writer.WriteLine($"{names[i]},{birthdates[i]}");
-            }
+            Console.WriteLine(name);
+        }
+
+        Queue<string> theQueue = new Queue<string>();
+        foreach (string name in theSortedSet)
+        {
+            theQueue.Enqueue(name);
+        }
+        Console.WriteLine("--- Queue ---");
+        while (theQueue.Count > 0)
+        {
+            Console.WriteLine(theQueue.Dequeue());
+        }
+
+        Stack<string> theStack = new Stack<string>();
+        foreach (string name in theSortedSet)
+        {
+            theStack.Push(name);
+        }
+        Console.WriteLine("--- Stack ---");
+        while (theStack.Count > 0)
+        {
+            Console.WriteLine(theStack.Pop());
         }
     }
 }
