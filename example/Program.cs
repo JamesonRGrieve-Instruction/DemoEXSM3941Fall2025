aab4f1b886e09648dbbde57d8fe288a53fcc3604
James Grieve <jamesonrgrieve@gmail.com>
2025-11-27 19:07:20 -0700

    Solution;

--- example/Program.cs
+++ example/Program.cs
@@ -6,77 +6,185 @@ public class Program
 {
     static void Main(string[] args)
     {
-        string[] names = new string[] { "Joe", "John", "Jane", "John", "Joe" };
+        string[] usernames = new string[3];
+        string[] passwords = new string[3];
+        DateTime[] timestamps = new DateTime[3];
+        List<string>[] words = new List<string>[3];
+
+        int logicalSize = 0;
+        string outerUserChoice = "";
+        do
+        {
+            // Login/Register Menu
+            Console.Clear();
+            Console.Write("Welcome to The Words Program!\n1. Login\n2. Register\n3. Exit\n\tSelection: ");
+            outerUserChoice = Console.ReadLine().Trim();
+
+            if (outerUserChoice == "1")
+            {
+                // Login
+                Console.Write("Please enter your username: ");
+                string username = Console.ReadLine().Trim();
+                Console.Write("Please enter your password: ");
+                string password = Console.ReadLine().Trim();
+                int userIndex = 0;
+                // This prevents additional iterations in the event we uncover a match. It would be a bit more maintainbale as a while loop, but I left it as a for loop to demonstrate that additional conditions can be added.
+                for (int i = 0; i <= logicalSize && !(usernames[i] == username && passwords[i] == password); i++)
+                {
+                    userIndex = i + 1;
+                }
+                // Since we set userIndex to i+1, the last iteration will set it to 1 above the logical size, meaning in that case, we found no matches.
+                if (userIndex > logicalSize)
+                {
+                    Console.WriteLine("Invalid credentials, returning to menu.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    string innerUserChoice = "";
+                    do
+                    {
+                        // User/Words Menu
+                        Console.Clear();
+                        Console.Write($"Welcome, {username}!\n1. View Words\n2. Add Word\n3. Clear Words\n4. Show Account Info\n5. Logout\n\tSelection: ");
+                        innerUserChoice = Console.ReadLine().Trim();
+                        if (innerUserChoice == "1")
+                        {
+                            // View
+                            foreach (string word in words[userIndex])
+                            {
+                                Console.WriteLine(word);
+                            }
+                            Console.ReadLine();
+                        }
+                        else if (innerUserChoice == "2")
+                        {
+                            // Add
+                            Console.Write("Please enter the word to add: ");
+                            string word = Console.ReadLine().Trim();
+                            if (word.Length > 0 && !word.Contains(' '))
+                            {
+                                words[userIndex].Add(word);
+                            }
+                            else
+                            {
+                                Console.WriteLine("No word or multiple words entered, returning to menu.");
+                            }
+                            Console.ReadLine();
+                        }
+                        else if (innerUserChoice == "3")
+                        {
+                            // Clear
+                            words[userIndex].Clear();
+                            Console.ReadLine();
+                        }
+                        else if (innerUserChoice == "4")
+                        {
+                            // Info
+                            Console.WriteLine($"User {usernames[userIndex]} created at {timestamps[userIndex].ToLongTimeString()} on {timestamps[userIndex].ToLongDateString()}. This was {DateTime.Now - timestamps[userIndex]} ago.");
+                            Console.ReadLine();
+                        }
+                        else if (innerUserChoice != "5")
+                        {
+                            Console.WriteLine("Invalid selection made, press enter to reload the menu.");
+                            Console.ReadLine();
+                        }
+                    } while (innerUserChoice != "5");
+                }
+            }
+            else if (outerUserChoice == "2")
+            {
+                // Register
+                if (logicalSize < 3)
+                {
+                    string username = GetValidUsername(usernames);
+                    string password = GetValidPassword(username);
+
+                    usernames[logicalSize] = username;
+                    passwords[logicalSize] = password;
+                    timestamps[logicalSize] = DateTime.Now;
+                    words[logicalSize] = new List<string>();
+
+                    logicalSize++;
+                }
+                else
+                {
+                    Console.WriteLine("Maximum users stored, returning to menu.");
+                    Console.ReadLine();
+                }
+            }
+            else if (outerUserChoice != "3")
+            {
+                Console.WriteLine("Invalid selection made, press enter to reload the menu.");
+                Console.ReadLine();
+            }
+        } while (outerUserChoice != "3");
 
 
-        // Automatically filter out duplicates.
-        HashSet<string> theHashSet = new HashSet<string>();
-
-        foreach (string name in names)
+    }
+    static string GetValidUsername(string[] existingUsernames)
+    {
+        string username;
+        bool valid = false;
+        do
         {
-            theHashSet.Add(name);
-        }
-        Console.WriteLine("--- HashSet ---");
-        foreach (string name in theHashSet)
+            Console.Write("Please enter your desired username: ");
+            username = Console.ReadLine().Trim();
+            valid = true;
+            if (string.IsNullOrWhiteSpace(username) || existingUsernames.Contains(username))
+            {
+                valid = false;
+            }
+            if (!valid)
+            {
+                Console.WriteLine("Your chosen username is invalid, it must not already exist and not be empty.");
+            }
+        } while (!valid);
+        return username;
+    }
+    static string GetValidPassword(string pairedUsername)
+    {
+        string password;
+        bool valid = false;
+        do
         {
-            Console.WriteLine(name);
-        }
-
-        // Automatically filter out duplicates and sort.
-        SortedSet<string> theSortedSet = new SortedSet<string>();
-
-        foreach (string name in names)
-        {
-            theSortedSet.Add(name);
-        }
-        Console.WriteLine("--- SortedSet ---");
-        foreach (string name in theSortedSet)
-        {
-            Console.WriteLine(name);
-        }
-
-        Queue<string> theQueue = new Queue<string>();
-        foreach (string name in theSortedSet)
-        {
-            theQueue.Enqueue(name);
-        }
-        Console.WriteLine("--- Queue ---");
-        while (theQueue.Count > 0)
-        {
-            Console.WriteLine(theQueue.Dequeue());
-        }
-
-        Stack<string> theStack = new Stack<string>();
-        foreach (string name in theSortedSet)
-        {
-            theStack.Push(name);
-        }
-        Console.WriteLine("--- Stack ---");
-        while (theStack.Count > 0)
-        {
-            Console.WriteLine(theStack.Pop());
-        }
-
-        Dictionary<string, string> theDictionary = new Dictionary<string, string>();
-        foreach (string name in theHashSet)
-        {
-            theDictionary.Add(name, "LastNameHere");
-        }
-        Console.WriteLine("--- Dictionary ---");
-        foreach (KeyValuePair<string, string> kvp in theDictionary)
-        {
-            Console.WriteLine($"{kvp.Key} {kvp.Value}");
-        }
-
-        SortedList<string, string> theSortedList = new SortedList<string, string>();
-        foreach (string name in theHashSet)
-        {
-            theSortedList.Add(name, "LastNameHere");
-        }
-        Console.WriteLine("--- SortedList ---");
-        foreach (KeyValuePair<string, string> kvp in theSortedList)
-        {
-            Console.WriteLine($"{kvp.Key} {kvp.Value}");
-        }
+            Console.Write("Please enter your desired password: ");
+            password = Console.ReadLine().Trim();
+            valid = true;
+            if (password.Length < 5)
+            {
+                valid = false;
+            }
+            if (pairedUsername == password)
+            {
+                valid = false;
+            }
+            bool containsDigit = false, containsUpper = false, containsLower = false, containsSymbol = false;
+            foreach (char character in password)
+            {
+                if (char.IsUpper(character))
+                {
+                    containsUpper = true;
+                }
+                if (char.IsLower(character))
+                {
+                    containsLower = true;
+                }
+                if (char.IsDigit(character))
+                {
+                    containsDigit = true;
+                }
+                if (char.IsSymbol(character))
+                {
+                    containsSymbol = true;
+                }
+            }
+            valid = valid && containsDigit && containsUpper && containsLower && containsSymbol;
+            if (!valid)
+            {
+                Console.WriteLine("Your chosen password is invalid, it must be at least 5 characters long containing at least one uppercase letter, lowercase letter, digit and symbol. It cannot be the same as the username.");
+            }
+        } while (!valid);
+        return password;
     }
 }
