c733b4d75fa5b186a3552e3d38cb1d2797490eca
James Grieve <jamesonrgrieve@gmail.com>
2025-11-27 19:22:56 -0700

    File IO and bonus;

--- example/Program.cs
+++ example/Program.cs
@@ -2,21 +2,54 @@
 
 namespace example;
 
+public struct User
+{
+    public string username;
+    public string password;
+    public DateTime timestamp;
+    public List<string> words;
+}
+
 public class Program
 {
     static void Main(string[] args)
     {
-        string[] usernames = new string[3];
-        string[] passwords = new string[3];
-        DateTime[] timestamps = new DateTime[3];
-        List<string>[] words = new List<string>[3];
-
+        User[] users = new User[3];
+        string line = "";
         int logicalSize = 0;
+        const string FILE_NAME = "users.csv";
+        // username,password,timestamp,word1,word2,word3...
+        try
+        {
+            using (StreamReader sr = new StreamReader(FILE_NAME))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    users[logicalSize] = new User()
+                    {
+                        username = values[0],
+                        password = values[1],
+                        timestamp = DateTime.Parse(values[2]),
+                        words = new List<string>()
+                    };
+                    for (int i = 3; i < values.Length; i++)
+                    {
+                        users[logicalSize].words.Add(values[i]);
+                    }
+                    logicalSize++;
+                }
+            }
+        }
+        catch (Exception e) { }
+
+
         string outerUserChoice = "";
+
         do
         {
             // Login/Register Menu
-            Console.Clear();
+            // Console.Clear();
             Console.Write("Welcome to The Words Program!\n1. Login\n2. Register\n3. Exit\n\tSelection: ");
             outerUserChoice = Console.ReadLine().Trim();
 
@@ -29,7 +62,7 @@ public class Program
                 string password = Console.ReadLine().Trim();
                 int userIndex = 0;
                 // This prevents additional iterations in the event we uncover a match. It would be a bit more maintainbale as a while loop, but I left it as a for loop to demonstrate that additional conditions can be added.
-                for (int i = 0; i <= logicalSize && !(usernames[i] == username && passwords[i] == password); i++)
+                for (int i = 0; i <= logicalSize && !(users[i].username == username && users[i].password == password); i++)
                 {
                     userIndex = i + 1;
                 }
@@ -45,13 +78,13 @@ public class Program
                     do
                     {
                         // User/Words Menu
-                        Console.Clear();
-                        Console.Write($"Welcome, {username}!\n1. View Words\n2. Add Word\n3. Clear Words\n4. Show Account Info\n5. Logout\n\tSelection: ");
+                        // Console.Clear();
+                        Console.Write($"Welcome, {username}!\n1. View Words\n2. Add Word\n3. Clear Words\n4. Show Account Info\n5. Change Password\n6. Logout\n\tSelection: ");
                         innerUserChoice = Console.ReadLine().Trim();
                         if (innerUserChoice == "1")
                         {
                             // View
-                            foreach (string word in words[userIndex])
+                            foreach (string word in users[userIndex].words)
                             {
                                 Console.WriteLine(word);
                             }
@@ -64,7 +97,8 @@ public class Program
                             string word = Console.ReadLine().Trim();
                             if (word.Length > 0 && !word.Contains(' '))
                             {
-                                words[userIndex].Add(word);
+                                users[userIndex].words.Add(word);
+                                SaveData(users, logicalSize, FILE_NAME);
                             }
                             else
                             {
@@ -75,16 +109,25 @@ public class Program
                         else if (innerUserChoice == "3")
                         {
                             // Clear
-                            words[userIndex].Clear();
+                            users[userIndex].words.Clear();
+                            SaveData(users, logicalSize, FILE_NAME);
                             Console.ReadLine();
                         }
                         else if (innerUserChoice == "4")
                         {
                             // Info
-                            Console.WriteLine($"User {usernames[userIndex]} created at {timestamps[userIndex].ToLongTimeString()} on {timestamps[userIndex].ToLongDateString()}. This was {DateTime.Now - timestamps[userIndex]} ago.");
+                            Console.WriteLine($"User {users[userIndex].username} created at {users[userIndex].timestamp.ToLongTimeString()} on {users[userIndex].timestamp.ToLongDateString()}. This was {DateTime.Now - users[userIndex].timestamp} ago.");
                             Console.ReadLine();
                         }
-                        else if (innerUserChoice != "5")
+                        else if (innerUserChoice == "5")
+                        {
+                            // Info
+                            users[userIndex].password = GetValidPassword(users[userIndex].username);
+                            SaveData(users, logicalSize, FILE_NAME);
+                            Console.WriteLine("Password updated!");
+                            Console.ReadLine();
+                        }
+                        else if (innerUserChoice != "6")
                         {
                             Console.WriteLine("Invalid selection made, press enter to reload the menu.");
                             Console.ReadLine();
@@ -97,15 +140,16 @@ public class Program
                 // Register
                 if (logicalSize < 3)
                 {
-                    string username = GetValidUsername(usernames);
+                    string username = GetValidUsername(users);
                     string password = GetValidPassword(username);
 
-                    usernames[logicalSize] = username;
-                    passwords[logicalSize] = password;
-                    timestamps[logicalSize] = DateTime.Now;
-                    words[logicalSize] = new List<string>();
+                    users[logicalSize].username = username;
+                    users[logicalSize].password = password;
+                    users[logicalSize].timestamp = DateTime.Now;
+                    users[logicalSize].words = new List<string>();
 
                     logicalSize++;
+                    SaveData(users, logicalSize, FILE_NAME);
                 }
                 else
                 {
@@ -122,7 +166,7 @@ public class Program
 
 
     }
-    static string GetValidUsername(string[] existingUsernames)
+    static string GetValidUsername(User[] existingUsers)
     {
         string username;
         bool valid = false;
@@ -131,7 +175,14 @@ public class Program
             Console.Write("Please enter your desired username: ");
             username = Console.ReadLine().Trim();
             valid = true;
-            if (string.IsNullOrWhiteSpace(username) || existingUsernames.Contains(username))
+            foreach (User user in existingUsers)
+            {
+                if (user.username == username)
+                {
+                    valid = false;
+                }
+            }
+            if (string.IsNullOrWhiteSpace(username))
             {
                 valid = false;
             }
@@ -186,5 +237,18 @@ public class Program
             }
         } while (!valid);
         return password;
+    }
+
+    static void SaveData(User[] data, int logicalSize, string fileName)
+    {
+        int written = 0;
+        using (StreamWriter sw = new StreamWriter(fileName))
+        {
+            do
+            {
+                sw.WriteLine($"{data[written].username},{data[written].password},{data[written].timestamp}{(data[written].words.Count > 0 ? "," : "")}{string.Join(',', data[written].words)}");
+                written++;
+            } while (written < logicalSize);
+        }
     }
 }
